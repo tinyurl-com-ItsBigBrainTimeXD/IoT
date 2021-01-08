8efc6f0b38d27a557cd0222f6686b05d5fc60b47
LeafGecko <theleafgecko@gmail.com>
2021-01-09 01:41:00 +0800

    remove motion, added servo function

--- arduino_ver1/arduino_ver1.ino
+++ arduino_ver1/arduino_ver1.ino
@@ -5,14 +5,15 @@ Ultrasonic ultrasonic(4); // include Seeed Studio ultrasonic ranger library
 #include <Wire.h>        // include Arduino Wire library
 
 #include <Servo.h>
+
 const int buzzer = 7;
+
 // defines variables
 long duration;
 int distance;
 
-int ledPin = 13;                // LED 
-int pirPin = 2;                 // PIR Out pin 
-int pirStat = 0;  
+int pbPin = 12;   // choose the input pin (for a pushbutton)
+int val = 0;     // variable for reading the pin status
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
@@ -20,10 +21,11 @@ Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
 
 void setup() {
-  pinMode(ledPin, OUTPUT);     
-  pinMode(pirPin, INPUT); 
+   
+  pinMode(pbPin, INPUT);    // declare pushbutton as input
 
   pinMode(buzzer, OUTPUT);
+  
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 
   Serial.begin(9600);
@@ -31,22 +33,42 @@ void setup() {
 
 void loop() {
 
-  seeed_ultrasonic();
-
-  motion_sensor();
+//  seeed_ultrasonic();
 
 //  buzzer_sound();
 
-//  servo1();
+
+  lid_close();
+  
+  Serial.println("Lid closed, waiting for PB");
+  button_read();
+  
+  lid_open();
+  Serial.println("Lid open, waiting for PB");
+  
+  button_read();
+
+  
 }
 
-void servo1() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+void button_read(){
+  val = digitalRead(pbPin);  // read input value
+  while (val == LOW) {
+    val = digitalRead(pbPin);  // read input value
+  }
+}
+
+
+void lid_close() {
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+}
+
+void lid_open() {
+  for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
@@ -60,18 +82,6 @@ void buzzer_sound() {
 }
 
 
-void motion_sensor() {
-   pirStat = digitalRead(pirPin); 
-   
-  if (pirStat == HIGH) {            // if motion detected
-    digitalWrite(ledPin, HIGH);  // turn LED ON
-  } 
-  else {
-    digitalWrite(ledPin, LOW); // turn LED OFF if we have no motion  
-  }
-}
-
-
 void seeed_ultrasonic() {
     long RangeInCentimeters;
     RangeInCentimeters = ultrasonic.read();
@@ -80,5 +90,4 @@ void seeed_ultrasonic() {
     Serial.print(RangeInCentimeters,DEC);
     Serial.print(" cm");
     Serial.println("");
-    
 }
