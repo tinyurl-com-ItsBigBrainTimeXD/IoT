a98571ebbdcb8766524b1e453600017b95fdbea6
Wen Junhua <wenjh1998@gmail.com>
2021-01-09 02:11:55 +0800

    Create Translation.py

---
+++ arduino_ver1/Translation.py
@@ -0,0 +1,54 @@
+import time
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import DistanceSensor, Buzzer, Servo, Device, Button
+
+
+factory = PiGPIOFactory()
+Device.pin_factory = factory
+ultrasonic = DistanceSensor(4)
+buzzer = Buzzer(7)
+servo = Servo(9)
+pb = Button(12)
+position = 0
+val = 0
+
+
+def loop():
+    lid_close()
+    button_read()
+    lid_open()
+    button_read()
+
+
+def button_read():
+    while not pb.is_active():
+        time.sleep(0.1)
+
+
+def lid_close():
+    for i in range(90):
+        servo.value = i / 90 - 1
+        time.sleep(0.15)
+
+
+def lid_open():
+    for i in range(90, 0, -1):
+        servo.value = - i / 90
+
+
+def buzzer_sound():
+    buzzer.on()
+    time.sleep(1)
+    buzzer.off()
+    time.sleep(1)
+
+
+def seeed_ultrasonic():
+    range_cm = ultrasonic.distance
+    time.sleep(0.15)
+
+
+if __name__ == '__main__':
+    while True:
+        loop()
+
