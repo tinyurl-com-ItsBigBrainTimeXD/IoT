927ab2088457c8ca2a63ccf911786a6176381bab
LeafGecko <theleafgecko@gmail.com>
2021-01-09 03:16:34 +0800

    Update Translation.py
    edited to test values

--- arduino_ver1/Translation.py
+++ arduino_ver1/Translation.py
@@ -2,21 +2,31 @@ import time
 from gpiozero import DistanceSensor, Buzzer, Servo, Button
 
 
-ultrasonic = DistanceSensor(4)
-buzzer = Buzzer(7)
-servo = Servo(9)
+ultrasonic = DistanceSensor(13)
+buzzer = Buzzer(17)
+servo = Servo(19)
 pb = Button(12)
 position = 0
 val = 0
 
 
 def loop():
+    
+    print("ultra is on, press to stop")
+    while button_read():
+        seeed_ultrasonic()
+        
+    print("buzzer is on, press to stop")
+    while button_read():
+        buzzer_sound()
+    
     lid_close()
     print("Lid closed, waiting for PB")
     button_read()
     lid_open()
     print("Lid open, waiting for PB")
     button_read()
+    seeed_ultrasonic()
 
 
 def button_read():
