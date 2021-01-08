525f4fce5c32a438c201ec0666de41ebfdb84a69
Wen Junhua <wenjh1998@gmail.com>
2021-01-09 01:21:11 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,10 +1,11 @@
 import multiprocessing as mp
+from time import sleep
 from Network.networking import send_data, form_packet
 
 def start_polling(input_queue: mp.Queue, output_queue: mp.Queue, host: str):
     """Start polling the server for data"""
     while True:
-
+        sleep(1)
         msg = form_packet('GET', host, '/frontend', {
             'type': 1
         })
@@ -18,7 +19,7 @@ def start_polling(input_queue: mp.Queue, output_queue: mp.Queue, host: str):
 if __name__ == "__main__":
     input_queue = mp.Queue()
     output_queue = mp.Queue()
-    proc = mp.Process(target = start_polling, args=(input_queue, output_queue, 'localhost'))
+    proc = mp.Process(target = start_polling, args=(input_queue, output_queue, '192.168.43.32'))
     proc.start()
 
     # Run Pi processes here
