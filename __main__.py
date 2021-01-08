26e5107e77d15fbbd4fce7f046e270afd649a781
Wen Junhua <wenjh1998@gmail.com>
2021-01-08 19:58:36 +0800

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,27 @@
+import multiprocessing as mp
+from Network.networking import send_data, form_packet
+
+def start_polling(input_queue: mp.Queue, output_queue: mp.Queue, host: str):
+    """Start polling the server for data"""
+    while True:
+
+        msg = form_packet('GET', host, '/frontend', {
+            'type': 1
+        })
+
+        if not input_queue.empty():
+            msg = input_queue.get()
+
+        _, _, content = send_data(msg)
+        output_queue.put(content)
+
+if __name__ == "__main__":
+    input_queue = mp.Queue()
+    output_queue = mp.Queue()
+    proc = mp.Process(target = start_polling, args=(input_queue, output_queue, 'localhost'))
+    proc.start()
+
+    # Run Pi processes here
+    while True:
+        if not output_queue.empty():
+            print(output_queue.get())
