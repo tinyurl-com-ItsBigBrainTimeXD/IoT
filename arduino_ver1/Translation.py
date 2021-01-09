c75eb253c1add2f33b539a49d509f955ce860458
LeafGecko <theleafgecko@gmail.com>
2021-01-09 09:22:50 +0800

    write warning oled

--- arduino_ver1/Translation.py
+++ arduino_ver1/Translation.py
@@ -1,6 +1,61 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+disp.begin()
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+# Load default font.
+font = ImageFont.load_default()
+
+def writeWarning(){
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    draw.text((x, top),       "Alarm Activated",  font=font, fill=255)
+    draw.text((x, top+8),     "Buzzer Sounding", font=font, fill=255)
+#     draw.text((x, top+16),    "",  font=font, fill=255)
+#     draw.text((x, top+25),    "",  font=font, fill=255)
+    disp.image(image)
+    disp.display()
+    time.sleep(5)
+}
 
 #Set warnings off (optional)
 GPIO.setwarnings(False)
@@ -71,7 +126,10 @@ def loop():
     SetAngle(0) #open
 
     sleep(5)
+    writeWarning()
+	
     SetAngle(90)
+    sleep(1)
     SetLock(90)
     sleep(5)
     
